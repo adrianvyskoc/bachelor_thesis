87dfdecf23bfabd77bad8a0e2003b4fce01863aa
Adrián Vyskoč <vyskoc.adrian@gmail.com>
2019-04-17 16:29:30 +0200

    exportovanie - export vsetkych tabuliek + kazda tabulka jeden sheet

--- bakalarka-frontend/src/app/plugins/utils/export.service.ts
+++ bakalarka-frontend/src/app/plugins/utils/export.service.ts
@@ -13,16 +13,59 @@ export class ExportService {
    * Funkcia na exportovanie viacerých tabuliek do excelu (xlsx).
    * @param tables - pole tabuliek, ktoré majú byť exportované do excelu
    * @param filename - názov vytvoreného súboru
+   * @param tableObjects - pole tabuliek, ktore chceme xportovat z pola objektov
+   *  Štruktura:
+   *  [
+   *    {
+   *      data: // data ktore exportujeme
+   *      attrs: // atributy, ktore chceme exportovať (ak neuvedieme, exportuju sa všetky)
+   *    },
+   *    ...
+   *  ]
+   * @param sheets - každá tabuľka na novom hárku, alebo nie (true - ano, false - nie)
    */
-  exportMultipleTablesToExcel(tables, filename) {
-    const toExport = Array.from(tables).reduce((acc, table: Element) => {
-      acc += table.querySelector("thead").outerHTML
-      acc += table.querySelector("tbody").outerHTML
-      acc += '<tr><td></td></tr>'
-      return acc
-    }, '')
+  exportMultipleTablesToExcel(tables, filename, tableObjects = [], sheets = true) {
+    let workbook
+    let i = 1
 
-    var workbook = XLSX.read('<table>' + toExport + '</table>', {type:'string'})
+    if(sheets) {
+
+      workbook = XLSX.utils.book_new()
+      tables.forEach((table) => {
+        let worksheet: XLSX.WorkSheet = XLSX.utils.table_to_sheet(table)
+
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet' + i++)
+      })
+
+      // preiterovanie objektov, ktore chceme exportovat do xlsx
+      tableObjects.forEach((tableObject) => {
+        // ak mame uveden atributy, ktore chceme exportovat, vyberieme ich, ak nie preskocime
+        if(tableObject.attrs) {
+          tableObject.data = tableObject.data.map(item => {
+            let newItem = {}
+            tableObject.attrs.forEach(attr => {
+              newItem[attr] = item[attr]
+            })
+            return newItem
+          })
+        }
+
+        let worksheet = XLSX.utils.json_to_sheet(tableObject.data)
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet' + i++)
+      })
+
+    } else {
+      const toExport = Array.from(tables).reduce((acc, table: Element) => {
+        acc += table.querySelector("thead").outerHTML
+        acc += table.querySelector("tbody").outerHTML
+        acc += '<tr><td></td></tr>'
+        return acc
+      }, '')
+
+      workbook = XLSX.read('<table>' + toExport + '</table>', {type:'string'})
+
+    }
+
     XLSX.writeFile(workbook, `${filename}.xlsx`)
   }
 
